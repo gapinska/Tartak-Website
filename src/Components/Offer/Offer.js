6f0b7cbd0ef47d966f798c739cb79a87c79259fe
Monika Gapińska <45268855+gapinska@users.noreply.github.com>
2020-08-31 21:18:53 +0200

    Update Offer.js

--- src/Components/Offer/Offer.js
+++ src/Components/Offer/Offer.js
@@ -2,22 +2,26 @@ import "./Offer.scss"
 import React, { Component } from "react"
 import products from "./../../assets/products.json"
 import ProductsList from "./ProductsList"
+import CategoryProducts from "./CategoryProducts"
 
-const productsData = products
+const productsData = products,
+  categoryProducts = products["category-products"]
 
 class Offer extends Component {
   state = {
-    products: productsData,
+    categoryProducts: categoryProducts,
   }
-
   render() {
+    console.log(categoryProducts)
     return (
-      <div className="products-container">
-        <ProductsList
-          products={this.state.products}
-          productsData={productsData}
-        />
-      </div>
+      <>
+        <div className="products-container">
+          <ProductsList productsData={productsData} />
+        </div>
+        <div className="category-products">
+          <CategoryProducts categoryProducts={this.state.categoryProducts} />
+        </div>
+      </>
     )
   }
 }
