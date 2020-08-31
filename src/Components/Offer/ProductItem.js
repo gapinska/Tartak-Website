66d0ec9cc22d72f72ae62e5141f115077fff50ec
Monika Gapińska <45268855+gapinska@users.noreply.github.com>
2020-08-31 21:20:48 +0200

    Update ProductItem.js

--- src/Components/Offer/ProductItem.js
+++ src/Components/Offer/ProductItem.js
@@ -1,11 +1,9 @@
-import React, { Component } from "react"
+import React from "react"
 
 const ProductItem = (props) => {
   const path = "./../../img/products"
-  console.log(props.productImg)
   return (
     <div className="product-container">
-      {/* <img src="products/highwaist-1.jpg" alt="" /> */}
       <img src={"products/" + props.productImg} alt="" />
       <header>{props.name}</header>
     </div>
