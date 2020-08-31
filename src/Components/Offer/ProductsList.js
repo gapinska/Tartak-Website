c63c4d1e2cf94b308bc23f73cbe876b99becff8e
Monika Gapińska <45268855+gapinska@users.noreply.github.com>
2020-08-31 21:20:07 +0200

    Update ProductsList.js

--- src/Components/Offer/ProductsList.js
+++ src/Components/Offer/ProductsList.js
@@ -3,7 +3,11 @@ import ProductItem from "./ProductItem"
 
 const ProductsList = (props) =>
   props.productsData.product.map((product) => (
-    <ProductItem name={product.name} productImg={product.productImg} />
+    <ProductItem
+      key={product.id}
+      name={product.name}
+      productImg={product.productImg}
+    />
   ))
 
 export default ProductsList
